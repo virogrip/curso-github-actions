237db5a82099e3abab29e2d7e850259697dbf91c
Albanez <albanezdev26@gmail.com>
2026-04-03 15:11:52 -0600

    add lenguaje-favorito.ts

---
+++ src/lenguaje-favorito.ts
@@ -0,0 +1,4 @@
+const nombre: string | undefined = process.env.USERNAME;
+const lenguajeFavorito: string | undefined = process.env.LANGUAGE;
+
+console.log(`¡Mi nombre es ${nombre} y mi lenguaje favorito es ${lenguajeFavorito}!`);
